9786b2b1e813075c363cd2ab74e0853ad471db9e
Jed Watson <jed.watson@me.com>
2014-12-03 12:04:42 +1100

    Updating package entrypoint

---
+++ index.js
@@ -0,0 +1,8 @@
+var Touchstone = {
+	createApp: require('./lib/createApp'),
+	Navigation: require('./lib/mixins/Navigation'),
+	Link: require('./lib/components/Link'),
+	UI: require('./lib/ui')
+};
+
+module.exports = Touchstone;
