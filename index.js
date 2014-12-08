33ff4b37f1cf7e805145d327d1be016bf136a533
Joss Mackison <joss.mackison@gmail.com>
2014-12-09 07:53:16 +1100

    Add missing dialog stuff

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Touchstone = {
 	createApp: require('./lib/createApp'),
 	Navigation: require('./lib/mixins/Navigation'),
+	Dialogs: require('./lib/mixins/Dialogs'),
 	Link: require('./lib/components/Link'),
 	UI: require('./lib/ui')
 };
