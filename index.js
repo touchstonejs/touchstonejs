bd0693232c46f6cf736455caaf22109fdf860e73
Jed Watson <jed.watson@me.com>
2014-12-10 17:14:03 +1100

    Update 2014-12-10T06:13:55.566Z

--- index.js
+++
@@ -1,9 +0,0 @@
-var Touchstone = {
-	createApp: require('./lib/createApp'),
-	Navigation: require('./lib/mixins/Navigation'),
-	Dialogs: require('./lib/mixins/Dialogs'),
-	Link: require('./lib/components/Link'),
-	UI: require('./lib/ui')
-};
-
-module.exports = Touchstone;
