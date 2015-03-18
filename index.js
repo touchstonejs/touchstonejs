c387c07d4b3d6f64059288e1febe65d0b8eb0623
marc <marc.hoeffl@gmail.com>
2015-03-18 15:25:18 +0100

    Expose Animation Mixin

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var Touchstone = {
 	createApp: require('./lib/createApp'),
 	Navigation: require('./lib/mixins/Navigation'),
 	Dialogs: require('./lib/mixins/Dialogs'),
+	Animation: require('./lib/mixins/Animation'),
 	Link: require('./lib/components/Link'),
 	UI: require('./lib/ui')
 };
