4866d98bc2b028e587e5fff186d5fa4663976e93
Daniel Cousens <github@dcousens.com>
2015-06-04 12:27:37 +1000

    PopupIcon: use oneOf to limit types
    Thanks @KeKs0r

--- src/ui/PopupIcon.js
+++ src/ui/PopupIcon.js
@@ -1,15 +1,15 @@
 var React = require('react/addons');
-
 var classNames = require('classnames');
 
 module.exports = React.createClass({
 	displayName: 'PopupIcon',
 	propTypes: {
 		name: React.PropTypes.string,
-		type: React.PropTypes.string,
+		type: React.PropTypes.oneOf(['default', 'muted', 'primary', 'success', 'warning', 'danger']),
 		spinning: React.PropTypes.bool
 	},
-	render: function() {
+
+	render: function () {
 		var className = classNames('Modal-icon', {
 			'is-spinning': this.props.spinning
 		}, this.props.name, this.props.type);
