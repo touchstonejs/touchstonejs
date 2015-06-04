e58c1ad27370fe24c4a1f61b04c1e62f9a9b8ad8
Daniel Cousens <github@dcousens.com>
2015-06-04 14:45:05 +1000

    Keypad: add missing semi-colons

--- src/ui/Keypad.js
+++ src/ui/Keypad.js
@@ -41,18 +41,18 @@ module.exports = React.createClass({
 
 		return (
 			<ViewContent className={keypadClassName}>
-				<KeypadButton action={function () { return action('1') }} primaryLabel="1" />
-				<KeypadButton action={function () { return action('2') }} primaryLabel="2" secondaryLabel="ABC" />
-				<KeypadButton action={function () { return action('3') }} primaryLabel="3" secondaryLabel="DEF" />
-				<KeypadButton action={function () { return action('4') }} primaryLabel="4" secondaryLabel="GHI" />
-				<KeypadButton action={function () { return action('5') }} primaryLabel="5" secondaryLabel="JKL" />
-				<KeypadButton action={function () { return action('6') }} primaryLabel="6" secondaryLabel="MNO" />
-				<KeypadButton action={function () { return action('7') }} primaryLabel="7" secondaryLabel="PQRS" />
-				<KeypadButton action={function () { return action('8') }} primaryLabel="8" secondaryLabel="TUV" />
-				<KeypadButton action={function () { return action('9') }} primaryLabel="9" secondaryLabel="WXYZ" />
+				<KeypadButton action={function () { return action('1'); }} primaryLabel="1" />
+				<KeypadButton action={function () { return action('2'); }} primaryLabel="2" secondaryLabel="ABC" />
+				<KeypadButton action={function () { return action('3'); }} primaryLabel="3" secondaryLabel="DEF" />
+				<KeypadButton action={function () { return action('4'); }} primaryLabel="4" secondaryLabel="GHI" />
+				<KeypadButton action={function () { return action('5'); }} primaryLabel="5" secondaryLabel="JKL" />
+				<KeypadButton action={function () { return action('6'); }} primaryLabel="6" secondaryLabel="MNO" />
+				<KeypadButton action={function () { return action('7'); }} primaryLabel="7" secondaryLabel="PQRS" />
+				<KeypadButton action={function () { return action('8'); }} primaryLabel="8" secondaryLabel="TUV" />
+				<KeypadButton action={function () { return action('9'); }} primaryLabel="9" secondaryLabel="WXYZ" />
 				{wildkey}
-				<KeypadButton action={function () { return action('0') }} primaryLabel="0" />
-				<KeypadButton action={function () { return action('delete') }} icon={icons.del} disabled={!this.props.enableDel} aux />
+				<KeypadButton action={function () { return action('0'); }} primaryLabel="0" />
+				<KeypadButton action={function () { return action('delete'); }} icon={icons.del} disabled={!this.props.enableDel} aux />
 			</ViewContent>
 		);
 	}
