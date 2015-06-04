f05f2e44061ce95169c370f62d1177d452f1d4e9
Daniel Cousens <github@dcousens.com>
2015-06-04 15:52:00 +1000

    gulpfile: simplify

--- gulpfile.js
+++ gulpfile.js
@@ -7,73 +7,34 @@ var git = require('gulp-git');
 var gulp = require('gulp');
 var less = require('gulp-less');
 
-var src = [
-	'src/**/*.js',
-	'!**/__tests__/**/*'
-];
-/**
- * Clean the build
- */
-
-gulp.task('clean:lib', function (done) {
-	del(['lib'], done);
-});
-
-/**
- * Build lib
- */
-
+// Build/Clean/Watch lib
 gulp.task('build:lib', function () {
-	return gulp.src(src)
-		.pipe(babel({
-			plugins: [require('babel-plugin-object-assign')]
-		}))
+	return gulp.src('src/**/*.js')
+		.pipe(babel({ plugins: [require('babel-plugin-object-assign')] }))
 		.pipe(gulp.dest('lib'));
 });
 
+gulp.task('clean:lib', function (done) { del(['lib'], done); });
 gulp.task('watch:lib', ['build:lib'], function () {
-	gulp.watch(src, ['build:lib']);
+	gulp.watch('src/**/*.js', ['build:lib']);
 });
 
-/**
- * Clean site build
- */
-
-gulp.task('clean:site', function (done) {
-	del(['site/public/build'], done);
+// Build/Clean/Watch site
+gulp.task('build:site', ['build:site:css']);
+gulp.task('clean:site', function (done) { del(['site/public/build'], done); });
+gulp.task('watch:site', ['build:site:css'], function () {
+	gulp.watch(['site/src/less/**/*'], ['build:site:css']);
 });
 
-/**
- * Build site css from less
- */
-
-function buildSiteCSS () {
+// Build CSS
+gulp.task('build:site:css', function () {
 	return gulp.src('site/src/less/site.less')
 		.pipe(less())
 		.pipe(gulp.dest('site/public/build/css'))
 		.pipe(connect.reload());
-}
+})
 
-gulp.task('build:site:css', buildSiteCSS);
-
-/**
- * Build site
- */
-
-gulp.task('build:site', [
-	'build:site:css'
-]);
-
-gulp.task('watch:site', [
-	'build:site:css'
-], function () {
-	gulp.watch(['site/src/less/**/*'], ['build:site:css']);
-});
-
-/**
- * Serve task for local development
- */
-
+// Local HTTP server
 gulp.task('site', ['watch:site'], function () {
 	connect.server({
 		root: 'site/public',
@@ -82,27 +43,7 @@ gulp.task('site', ['watch:site'], function () {
 	});
 });
 
-/**
- * Version bump tasks
- */
-
-function _bump (type) {
-	return function () {
-		return gulp.src(['./package.json', './bower.json'])
-			.pipe(bump({ type: type }))
-			.pipe(gulp.dest('./'));
-	};
-}
-
-gulp.task('bump', _bump('patch'));
-gulp.task('bump:minor', _bump('minor'));
-gulp.task('bump:major', _bump('major'));
-
-/**
- * Git tag task
- * (version *must* be bumped first)
- */
-
+// git tag
 gulp.task('publish:tag', function (done) {
 	var pkg = require('./package.json');
 	var v = 'v' + pkg.version;
@@ -110,28 +51,30 @@ gulp.task('publish:tag', function (done) {
 
 	git.tag(v, message, function (err) {
 		if (err) throw err;
-		git.push('origin', v, function (err) {
-			if (err) throw err;
-			done();
-		});
+
+		git.push('origin', v, done)
 	});
 });
 
-/**
- * npm publish task
- * * (version *must* be bumped first)
- */
+// Version
+function _bump (type) {
+	return gulp.src(['./package.json', './bower.json'])
+		.pipe(bump({ type: type }))
+		.pipe(gulp.dest('./'));
+}
 
+gulp.task('bump', _bump.bind(null, 'patch'));
+gulp.task('bump:minor', _bump.bind(null, 'minor'));
+gulp.task('bump:major', _bump.bind(null, 'major'));
+
+// publish
 gulp.task('publish:npm', function (done) {
 	require('child_process')
 		.spawn('npm', ['publish'], { stdio: 'inherit' })
 		.on('close', done);
 });
 
-/**
- * Deploy tasks
- */
-
+// Deploy
 gulp.task('publish:site', ['build:site'], function () {
 	return gulp.src('site/public/**/*').pipe(deploy());
 });
