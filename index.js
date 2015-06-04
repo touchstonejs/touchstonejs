ab0624aebb05abf3b6864d3290598f4918f9691e
Daniel Cousens <github@dcousens.com>
2015-06-04 15:52:35 +1000

    index: remove unnecessary Touchstone variable

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-var Touchstone = {
+module.exports = {
 	createApp: require('./lib/createApp'),
 	Navigation: require('./lib/mixins/Navigation'),
 	Link: require('./lib/components/Link'),
 	UI: require('./lib/ui')
 };
-
-module.exports = Touchstone;
