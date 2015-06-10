7e53efb4523172a4a4b5ba23f8c45de7024d5fd0
Daniel Cousens <github@dcousens.com>
2015-06-10 13:49:35 +1000

    LabelInput: rm unused propTypes

--- src/ui/LabelInput.js
+++ src/ui/LabelInput.js
@@ -4,36 +4,32 @@ var classnames = require('classnames');
 
 module.exports = React.createClass({
 	displayName: 'LabelInput',
+
 	propTypes: {
-		className: React.PropTypes.string,
-		onChange: React.PropTypes.func,
-		type: React.PropTypes.string,
-		label: React.PropTypes.string,
-		pattern: React.PropTypes.string,
-		placeholder: React.PropTypes.string,
-		ref: React.PropTypes.string,
 		alignTop: React.PropTypes.bool,
-		readOnly: React.PropTypes.bool,
+		className: React.PropTypes.string,
 		disabled: React.PropTypes.bool,
-		first: React.PropTypes.bool
+		first: React.PropTypes.bool,
+		label: React.PropTypes.string,
+		readOnly: React.PropTypes.bool,
+		value: React.PropTypes.string
 	},
+
 	getDefaultProps: function () {
 		return {
 			type: 'text',
 			readOnly: false
 		};
 	},
+
 	render: function () {
-		var className = classnames(this.props.className, {
-			'list-item': true,
-			'field-item': true,
-			'is-first': this.props.first,
+		var className = classnames(this.props.className, 'list-item', 'field-item', {
 			'align-top': this.props.alignTop,
+			'is-first': this.props.first,
 			'u-selectable': this.props.disabled
 		});
 
 		var props = blacklist(this.props, 'alignTop', 'children', 'first', 'readOnly');
-
 		var renderInput = this.props.readOnly ? (
 			<div className="field u-selectable">{this.props.value}</div>
 		) : (
