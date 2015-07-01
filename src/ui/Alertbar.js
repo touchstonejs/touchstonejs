152b971935f6bdf41bea61da96bb1c64862d4d1e
Daniel Cousens <github@dcousens.com>
2015-07-01 20:25:38 +1000

    cleanup Alertbar

--- src/ui/Alertbar.js
+++ src/ui/Alertbar.js
@@ -1,33 +1,37 @@
-var classnames = require('classnames');
 var React = require('react/addons');
-var ViewContent = require('./ViewContent');
+
+var blacklist = require('blacklist');
+var classnames = require('classnames');
 
 module.exports = React.createClass({
 	displayName: 'Alertbar',
 	propTypes: {
-		children: React.PropTypes.node,
+		children: React.PropTypes.node.isRequired,
 		className: React.PropTypes.string,
-		height: React.PropTypes.string,
 		pulse: React.PropTypes.bool,
 		type: React.PropTypes.oneOf(['default', 'primary', 'success', 'warning', 'danger'])
 	},
+
 	getDefaultProps () {
 		return {
-			height: '30px',
 			type: 'default'
-		};
+		}
 	},
-	render () {
-		var className = classnames(this.props.className, this.props.type, {
-			'Alertbar': true,
-			'pulse': this.props.pulse
-		});
-		var content = this.props.pulse ? <div className="Alertbar-inner">{this.props.children}</div> : this.props.children;
 
-		return (
-			<ViewContent height={this.props.height} className={className}>
-				{content}
-			</ViewContent>
-		);
+	render () {
+		var { pulse } = this.props
+		var className = classnames('Alertbar', this.props.className, this.props.type, { 'pulse': pulse });
+
+		if (pulse) {
+			var otherProps = blacklist(this.props, 'children')
+
+			return (
+				<div className={className} {... otherProps}>
+					<div className="Alertbar__inner">{this.props.children}</div>
+				</div>
+			)
+		}
+
+		return <div className={className} {...this.props} />
 	}
 });
