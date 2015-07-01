6ac90ea7c80fb46e10ac7b2862096eaaf2ca3972
Daniel Cousens <github@dcousens.com>
2015-07-01 23:26:52 +1000

    Tabs: remove complex cloning/context/select props pass through, mostly unnecessary

--- src/ui/Tabs.js
+++ src/ui/Tabs.js
@@ -1,64 +1,33 @@
-var blacklist = require('blacklist');
-var classnames = require('classnames');
 var React = require('react');
 var Tappable = require('react-tappable');
 
+var blacklist = require('blacklist')
+var classnames = require('classnames');
+
 export var Navigator = React.createClass({
-	propTypes: {
-		children: React.PropTypes.node,
-		onChange: React.PropTypes.func,
-		value: React.PropTypes.string
-	},
 	render () {
-		return (
-			<div className="Tabs-Navigator">
-				{React.Children.map(this.props.children, (tab) => {
-					return React.cloneElement(tab, {
-						onSelect: this.props.onChange,
-						navigatorValue: this.props.value
-					});
-				})}
-			</div>
-		);
+		var className = classnames('Tabs-Navigator', this.props.className)
+		var otherProps = blacklist(this.props, 'className')
+
+		return <div className={className} { ... otherProps } />
 	}
-});
+})
 
 export var Tab = React.createClass({
-	contextTypes: {
-		tabNavigator: React.PropTypes.object
-	},
 	propTypes: {
-		children: React.PropTypes.node,
-		navigatorValue: React.PropTypes.string,
-		onSelect: React.PropTypes.func,
-		value: React.PropTypes.string
+		selected: React.PropTypes.bool
 	},
-	onSelect () {
-		var tab = blacklist(this.props, 'children');
-		this.props.onSelect(tab);
-	},
+
 	render () {
-		var isCurrent = this.props.navigatorValue === this.props.value;
-		var className = classnames('Tabs-Tab', {
-			'is-selected': isCurrent
-		});
-		return (
-			<Tappable onTap={this.props.onSelect && this.onSelect} className={className}>
-				{this.props.children}
-			</Tappable>
-		);
+		var className = classnames('Tabs-Tab', { 'is-selected': this.props.selected });
+		var otherProps = blacklist(this.props, 'selected')
+
+		return <Tappable className={className} { ... otherProps } />
 	}
 });
 
 export var Label = React.createClass({
-	propTypes: {
-		children: React.PropTypes.node
-	},
 	render () {
-		return (
-			<div className="Tabs-Label">
-				{this.props.children}
-			</div>
-		);
+		return <div className="Tabs-Label" { ... this.props }/>
 	}
 });
