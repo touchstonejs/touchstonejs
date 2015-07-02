68929249e0d7a5c46fa59fd9c399913269e0f6c7
Joss Mackison <joss.mackison@gmail.com>
2015-07-02 16:44:39 +0200

    progress commit: mass component addition 2

---
+++ src/ui/Textarea.js
@@ -0,0 +1,32 @@
+var blacklist = require('blacklist');
+var Item = require('./Item');
+var ItemContent = require('./ItemContent');
+var ItemInner = require('./ItemInner');
+var React = require('react/addons');
+
+module.exports = React.createClass({
+	displayName: 'Input',
+	propTypes: {
+		children: React.PropTypes.node,
+		disabled: React.PropTypes.bool
+	},
+	getDefaultProps () {
+		return {
+			rows: 3
+		};
+	},
+	render () {
+		var inputProps = blacklist(this.props, 'children', 'className');
+
+		return (
+			<Item selectable={this.props.disabled} className={this.props.className} component="label">
+				<ItemInner>
+					<ItemContent component="label">
+						<textarea className="field" {...inputProps} />
+					</ItemContent>
+					{this.props.children}
+				</ItemInner>
+			</Item>
+		);
+	}
+});
