d05e970329aa4ad3be0680ee9d00e21362cbcf2b
Daniel Cousens <github@dcousens.com>
2015-07-03 19:59:55 +1000

    Tabs: add missing propTypes

--- src/ui/Tabs.js
+++ src/ui/Tabs.js
@@ -1,17 +1,21 @@
 var React = require('react');
 var Tappable = require('react-tappable');
 
-var blacklist = require('blacklist')
+var blacklist = require('blacklist');
 var classnames = require('classnames');
 
 export var Navigator = React.createClass({
+	propTypes: {
+		className: React.PropTypes.string
+	},
+
 	render () {
 		var className = classnames('Tabs-Navigator', this.props.className);
 		var otherProps = blacklist(this.props, 'className');
 
 		return <div className={className} { ... otherProps } />;
 	}
-})
+});
 
 export var Tab = React.createClass({
 	propTypes: {
