a25b2c58b27a49b7d489bbcf4a09c6c850844427
Joss Mackison <joss.mackison@gmail.com>
2015-07-23 12:27:46 +1000

    major improvements to SearchField component

--- src/ui/SearchField.js
+++ src/ui/SearchField.js
@@ -3,6 +3,7 @@ var classnames = require('classnames');
 var React = require('react/addons');
 var Tappable = require('react-tappable');
 
+
 module.exports = React.createClass({
 	displayName: 'SearchField',
 	propTypes: {
@@ -10,6 +11,7 @@ module.exports = React.createClass({
 		onChange: React.PropTypes.func,
 		onClear: React.PropTypes.func,
 		placeholder: React.PropTypes.string,
+		type: React.PropTypes.oneOf(['default', 'dark']),
 		value: React.PropTypes.string
 	},
 
@@ -21,6 +23,7 @@ module.exports = React.createClass({
 
 	getDefaultProps () {
 		return {
+			type: 'default',
 			value: ''
 		}
 	},
@@ -51,6 +54,12 @@ module.exports = React.createClass({
 		});
 	},
 
+	handleSubmit (e) {
+		e.preventDefault();
+		let input = React.findDOMNode(this.refs.input)
+		this.props.onSubmit(input.value);
+	},
+
 	renderClear () {
 		if (!this.props.value.length) return;
 		return <Tappable className="SearchField__icon SearchField__icon--clear" onTap={this.handleClear} />;
@@ -64,24 +73,24 @@ module.exports = React.createClass({
 	},
 
 	render () {
-		var className = classnames('SearchField', {
+		var className = classnames('SearchField', ('SearchField--' + this.props.type), {
 			'is-focused': this.state.isFocused,
 			'has-value': this.props.value
 		}, this.props.className);
 		var props = blacklist(this.props, 'className', 'placeholder', 'type');
 
 		return (
-			<div className={className}>
+			<form onSubmit={this.handleSubmit} action="javascript:;" className={className}>
 				<label className="SearchField__field">
 					<div className="SearchField__placeholder">
 						<span className="SearchField__icon SearchField__icon--search" />
 						{!this.props.value.length ? this.props.placeholder : null}
 					</div>
-					<input ref="input" value={this.props.value} onChange={this.handleChange} onFocus={this.handleFocus} onBlur={this.handleBlur} className="SearchField__input" />
+					<input type="search" ref="input" value={this.props.value} onChange={this.handleChange} onFocus={this.handleFocus} onBlur={this.handleBlur} className="SearchField__input" />
 					{this.renderClear()}
 				</label>
 				{this.renderCancel()}
-			</div>
+			</form>
 		);
 	}
 });
